894f75d41e1e5487651807186082f5ba9c258b4a
taniguchikouichi <kou.gtrnt0610@icloud.com>
2019-11-09 11:09:44 +0000

    label

--- app/controllers/admin/labels_controller.rb
+++ app/controllers/admin/labels_controller.rb
@@ -1,13 +1,25 @@
 class Admin::LabelsController < ApplicationController
     def new
+        @label = Label.new
     end
 
     def index
+        @labels = Label.all
     end
 
     def create
+        @label = Label.new(label_params)
+        @lavel.save
     end
 
     def destroy
+        @label = Label.find(params[:id])
+        @label.destroy
+        redirect_to admin_labels_path
+    end
+
+    private
+    def label_params
+        params.require(:label).permit(:label_name)
     end
 end
