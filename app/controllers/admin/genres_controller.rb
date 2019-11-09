b9779150741e7a964edb44a885d10e74c7ae48c4
taniguchikouichi <kou.gtrnt0610@icloud.com>
2019-11-09 13:29:30 +0000

    genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,28 @@
 class Admin::GenresController < ApplicationController
     def new
+        @genre = Genre.new
     end
 
     def index
+        @genres = Genre.all
     end
 
     def create
+        @genre = Genre.new(genre_params)
+        if @genre.save
+            redirect_to admin_genres_path
+        else
+            render :new
+        end
     end
 
     def destroy
+        genre = Genre.find(params[:id])
+        genre.destroy
+        redirect_to admin_genres_path
+    end
+    private
+    def genre_params
+        params.require(:genre).permit(:genre_name)
     end
 end
