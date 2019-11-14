df9039f4ceb02eb86ee8b6511b1f980e60deff27
taniguchikouichi <kou.gtrnt0610@icloud.com>
2019-11-14 06:25:53 +0000

    admin_novel

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,5 @@
 class Genre < ApplicationRecord
+    has_many :genre_combinations
+    has_many :novels, through: :genre_combinations
+    accepts_nested_attributes_for :genre_combinations, reject_if: :all_blank　, allow_destroy: true
 end
