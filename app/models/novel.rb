ff85760a2214d834923a32373631035299578fa2
taniguchikouichi <kou.gtrnt0610@icloud.com>
2019-11-21 09:35:58 +0000

    rank

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -6,7 +6,33 @@ class Novel < ApplicationRecord
     has_many :genres, through: :genre_combinations
     accepts_nested_attributes_for :genre_combinations, reject_if: :all_blank, allow_destroy: true
 
+    attr_accessor :average
+
+    def average_story
+        self.reviews.sum(:story_rate) / self.reviews.length
+    end
+
+    def average_production
+        self.reviews.sum(:production_rate) / self.reviews.length
+    end
+
+    def average_person
+        self.reviews.sum(:person_rate) / self.reviews.length
+    end
+
+    def average_setting
+        self.reviews.sum(:setting_rate) / self.reviews.length
+    end
+
+    def total_average
+        total_average = (average_story + average_production + average_person + average_setting)/4
+    end
+
+
     def favorited_by?(user) #いいねしているかどうか
         favorites.where(user_id: user.id).exists?
     end
+
+    
+
 end
